f3c0a1af6c70952cb44af326aa40053d5148fc53
skjanani <41111202+skjanani@users.noreply.github.com>
2018-08-15 10:17:04 +0530

    Add files via upload

---
+++ Charposition.java
@@ -0,0 +1,16 @@
+package janani;
+import java.util.Scanner;
+public class Charposition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String str;
+System.out.println("enter the string");
+Scanner ja=new Scanner(System.in);
+str=ja.next();
+char ch=str.charAt(0);
+int a=(int)ch;
+System.out.println(a);
+	}
+
+}
